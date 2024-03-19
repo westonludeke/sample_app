d1ab3c591770d26b71fbc9b8d4b6647914e2a1be
westonludeke <westonludeke@gmail.com>
2024-03-19 16:33:49 -0500

    add useState to App.js to display user's IP address and browser stack data on webpage for testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import html2canvas from "html2canvas";
 import UAParser from "ua-parser-js";
 
 const App = () => {
+  const [ipAddress, setIpAddress] = useState('');
+  const [browserInfo, setBrowserInfo] = useState({});
+
   useEffect(() => {
     // fetch user's IP Address
     fetch("https://api.ipify.org?format=json")
       .then((response) => response.json())
       .then((data) => {
         console.log("IP Address log:", data.ip);
+        setIpAddress(data.ip);
       })
       .catch((error) => {
         console.error("Error fetching IP address:", error);
@@ -18,6 +22,7 @@ const App = () => {
     const parser = new UAParser();
     const result = parser.getResult();
     console.log(result);
+    setBrowserInfo(result);
   }, []);
 
   // take screenshot
@@ -53,6 +58,15 @@ const App = () => {
       <button id="captureBtn" onClick={captureScreen}>
         Capture
       </button>
+
+      {/* Display IP Address and Browser Info */}
+      <div>
+        <p>IP Address: {ipAddress}</p>
+        <p>Browser Name: {browserInfo.browser?.name}</p>
+        <p>Browser Version: {browserInfo.browser?.version}</p>
+        <p>Operating System: {browserInfo.os?.name}</p>
+        <p>OS Version: {browserInfo.os?.version}</p>
+      </div>
     </div>
   );
 };
