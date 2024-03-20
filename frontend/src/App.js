2253a72767e1f9e135e323d4b6f69f6dbfafb0b6
westonludeke <westonludeke@gmail.com>
2024-03-20 10:45:56 -0500

    update App.js to swap html2canvas for react-screen-capture; add basic CSS to App.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,23 @@
-import React, { useEffect, useState } from "react";
-import html2canvas from "html2canvas";
+import React, { Component } from "react";
+import { ScreenCapture } from 'react-screen-capture';
 import UAParser from "ua-parser-js";
+import './App.css';
 
-const App = () => {
-  const [ipAddress, setIpAddress] = useState('');
-  const [browserInfo, setBrowserInfo] = useState({});
+class App extends Component {
+  state = {
+    screenCapture: '',
+    ipAddress: '',
+    browserInfo: {},
+    name: '',
+  };
 
-  useEffect(() => {
+  componentDidMount() {
     // fetch user's IP Address
     fetch("https://api.ipify.org?format=json")
       .then((response) => response.json())
       .then((data) => {
         console.log("IP Address log:", data.ip);
-        setIpAddress(data.ip);
+        this.setState({ ipAddress: data.ip });
       })
       .catch((error) => {
         console.error("Error fetching IP address:", error);
@@ -22,52 +27,67 @@ const App = () => {
     const parser = new UAParser();
     const result = parser.getResult();
     console.log(result);
-    setBrowserInfo(result);
-  }, []);
+    this.setState({ browserInfo: result });
+  }
 
-  // take screenshot
-  const captureScreen = () => {
-    html2canvas(document.body).then((canvas) => {
-      canvas.toBlob((blob) => {
-        const url = URL.createObjectURL(blob);
-        window.open(url, "_blank");
-        URL.revokeObjectURL(url);
-      }, "image/png");
-
-      const imgData = canvas.toDataURL("image/png");
-      console.log("Screenshot:", imgData);
-    });
+  handleScreenCapture = screenCapture => {
+    this.setState({ screenCapture });
   };
 
-  return (
-    <div>
-      <h2>Hello World!</h2>
-      <p>Testing 123</p>
-      <form id="testForm">
-        <label htmlFor="nameInput">Enter your name:</label>
-        <input type="text" id="nameInput" name="name" />
-      </form>
+  handleSave = () => {
+    const screenCaptureSource = this.state.screenCapture;
+    const downloadLink = document.createElement('a');
+    const fileName = 'react-screen-capture.png';
 
-      <ul>
-        <li>Test Item 1</li>
-        <li>Test Item 2</li>
-        <li>Test Item 3</li>
-      </ul>
+    downloadLink.href = screenCaptureSource;
+    downloadLink.download = fileName;
+    downloadLink.click();
+  };
 
-      <button id="captureBtn" onClick={captureScreen}>
-        Capture
-      </button>
+  handleInputChange = (event) => {
+    this.setState({ name: event.target.value });
+  };
 
-      {/* Display IP Address and Browser Info */}
-      <div>
-        <p>IP Address: {ipAddress}</p>
-        <p>Browser Name: {browserInfo.browser?.name}</p>
-        <p>Browser Version: {browserInfo.browser?.version}</p>
-        <p>Operating System: {browserInfo.os?.name}</p>
-        <p>OS Version: {browserInfo.os?.version}</p>
-      </div>
-    </div>
-  );
-};
+  render() {
+    const { screenCapture, ipAddress, browserInfo, name } = this.state;
+
+    return (
+      <ScreenCapture onEndCapture={this.handleScreenCapture}>
+        {({ onStartCapture }) => (
+          <div>
+            <h2>Hello World!</h2>
+            <p>Testing 123</p>
+            <form id="testForm">
+              <label htmlFor="nameInput">Enter your name:</label>
+              <input 
+                type="text"
+                id="nameInput"
+                name="name"
+                value={name}
+                onChange={this.handleInputChange}
+              />
+            </form>
+            
+            <button onClick={onStartCapture}>screenshot</button>
+
+            {/* Display IP Address and Browser Info */}
+            <div>
+              <p>IP Address: {ipAddress}</p>
+              <p>Browser Name: {browserInfo.browser?.name}</p>
+              <p>Browser Version: {browserInfo.browser?.version}</p>
+              <p>Operating System: {browserInfo.os?.name}</p>
+              <p>OS Version: {browserInfo.os?.version}</p>
+            </div>
+
+            <center>
+              <img src={screenCapture} alt='react-screen-capture' />
+              {screenCapture && <button onClick={this.handleSave}>download screenshot</button>}
+            </center>
+          </div>
+        )}
+      </ScreenCapture>
+    );
+  }
+}
 
 export default App;
